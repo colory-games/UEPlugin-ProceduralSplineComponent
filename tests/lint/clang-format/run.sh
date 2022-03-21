be05fe02a8778485d88190b7b5ef78e38606ee12
nutti <nutti.metro@gmail.com>
2022-03-21 02:13:22 +0000

    Add: linter tests for C# source code files

--- tests/lint/clang-format/run.sh
+++ tests/lint/clang-format/run.sh
@@ -10,7 +10,7 @@ readonly SOURCE_DIRECTORY=${1}
 readonly LINTER_CMD="clang-format"
 
 error=0
-for file in `find ${SOURCE_DIRECTORY} -iname "*.h" -o -iname "*.cpp" | sort`; do
+for file in `find ${SOURCE_DIRECTORY} -iname "*.h" -o -iname "*.cpp" -o -iname "*.cs" | sort`; do
     diff -u ${file} <(${LINTER_CMD} -style=file ${file})
     if [ ${?} -ne 0 ]; then
         ((error+=1))
