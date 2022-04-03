37a245ecf2114a84fff6ecc5c11bc514fd97f320
nutti <nutti.metro@gmail.com>
2022-04-03 23:44:03 +0900

    Add: Category of the UCircleSplineComponent's UPROPERTY

--- ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/CircleSplineComponent.h
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/CircleSplineComponent.h
@@ -12,9 +12,9 @@ class PROCEDURALSPLINECOMPONENT_API UCircleSplineComponent : public UProceduralS
 public:
 	virtual void ProceedSpline() override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 3))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Circle Spline Component", meta = (ClampMin = 3))
 	int32 PointNum = 30;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 0.0f))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Circle Spline Component", meta = (ClampMin = 0.0f))
 	float Radius = 500.0f;
 };
