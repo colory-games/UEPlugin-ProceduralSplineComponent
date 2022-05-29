a1e901f4f580daf42f71b22f42d74563d2fa0e19
beisaku <98695390+beisaku@users.noreply.github.com>
2022-05-29 22:44:41 +0900

    Add: ArcSplineComponent (#12)

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/ArcSplineComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ProceduralSplineComponentBase.h"
+
+#include "ArcSplineComponent.generated.h"
+
+UCLASS(ClassGroup = (ProceduralSpline), meta = (BlueprintSpawnableComponent))
+class PROCEDURALSPLINECOMPONENT_API UArcSplineComponent : public UProceduralSplineComponentBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ProceedSpline() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 3))
+	int32 PointNum = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 0.0f))
+	float Radius = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = -360.0f, ClampMax = 360.0f))
+	float StartAngle = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = -360.0f, ClampMax = 360.0f))
+	float EndAngle = 180.0f;
+};
