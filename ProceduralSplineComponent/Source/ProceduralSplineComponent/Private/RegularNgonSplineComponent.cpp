4a954015d99d15eba74bec20b30727dc1303f239
beisakubei <beisaku1toriaezu@gmail.com>
2022-05-29 16:01:13 +0900

    Modify Tabs

--- ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/RegularNgonSplineComponent.cpp
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/RegularNgonSplineComponent.cpp
@@ -6,15 +6,15 @@ void URegularNgonSplineComponent::ProceedSpline()
 
 	ClearSplinePoints();
 
-    InitRoundedCornerParam(RoundedCorner);
+	InitRoundedCornerParam(RoundedCorner);
 
-    TArray<FVector> NgonVertexLocations;
-    for (int VertexIndex = 0; VertexIndex < VertexNum; VertexIndex++)
-    {
-        const float IntervalAngleRad = 2.0f * PI / VertexNum;
-        NgonVertexLocations.Add(
-            FVector(Radius * cos(VertexIndex * IntervalAngleRad), Radius * sin(VertexIndex * IntervalAngleRad), 0.0f));
-    }
+	TArray<FVector> NgonVertexLocations;
+	for (int VertexIndex = 0; VertexIndex < VertexNum; VertexIndex++)
+	{
+		const float IntervalAngleRad = 2.0f * PI / VertexNum;
+		NgonVertexLocations.Add(
+			FVector(Radius * cos(VertexIndex * IntervalAngleRad), Radius * sin(VertexIndex * IntervalAngleRad), 0.0f));
+	}
 
-    MakeRoundedCornerSpline(NgonVertexLocations, bRoundedCorner, RoundedCorner);
+	MakeRoundedCornerSpline(NgonVertexLocations, bRoundedCorner, RoundedCorner);
 }
