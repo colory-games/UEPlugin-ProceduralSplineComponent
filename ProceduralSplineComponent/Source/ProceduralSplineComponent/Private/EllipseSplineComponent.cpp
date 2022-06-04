19e7fa7de2eec6d463a6ae99c815be084efc4024
beisakubei <beisaku1toriaezu@gmail.com>
2022-06-04 18:51:32 +0900

    Modify format

--- ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/EllipseSplineComponent.cpp
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/EllipseSplineComponent.cpp
@@ -8,8 +8,8 @@ void UEllipseSplineComponent::ProceedSpline()
 
 	for (int32 PointIndex = 0; PointIndex < PointNum; PointIndex++)
 	{
-		FVector Location = FVector(Width / 2.0f * FMath::Sin((float)PointIndex / PointNum * 2.0 * PI),
-			Length / 2.0f * FMath::Cos((float)PointIndex / PointNum * 2.0 * PI), 0.0f);
+		FVector Location = FVector(Width / 2.0f * FMath::Sin((float) PointIndex / PointNum * 2.0 * PI),
+			Length / 2.0f * FMath::Cos((float) PointIndex / PointNum * 2.0 * PI), 0.0f);
 		AddSplinePointAtIndex(Location, PointIndex, ESplineCoordinateSpace::Local, false);
 		SetSplinePointType(PointIndex, ESplinePointType::Curve, false);
 
