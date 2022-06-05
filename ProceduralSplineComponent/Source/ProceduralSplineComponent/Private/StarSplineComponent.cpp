ed4f0f9523e1f558d09961aabeac5aca3a457b61
beisakubei <beisaku1toriaezu@gmail.com>
2022-06-05 21:25:30 +0900

    Add star spline

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/StarSplineComponent.cpp
@@ -0,0 +1,26 @@
+#include "StarSplineComponent.h"
+
+void UStarSplineComponent::ProceedSpline()
+{
+	Super::ProceedSpline();
+
+	ClearSplinePoints();
+
+	InitRoundedCornerParam(RoundedCornerParams);
+
+	TArray<FVector> StarVertexLocations;
+	const int PointNum = Ngram * 2;
+	for (int32 PointIndex = 0; PointIndex < PointNum; PointIndex++)
+	{
+		const float Radius = (PointIndex % 2 == 0) ? OuterRadius : InnerRadius;
+		FVector Location = FVector(Radius * FMath::Cos((float) PointIndex / PointNum * 2.0 * PI),
+			-Radius * FMath::Sin((float) PointIndex / PointNum * 2.0 * PI), 0.0f);
+		StarVertexLocations.Add(Location);
+	}
+
+	MakeRoundedCornerSpline(StarVertexLocations, bRoundCorner, RoundedCornerParams);
+
+	SetClosedLoop(true, false);
+
+	UpdateSpline();
+}
