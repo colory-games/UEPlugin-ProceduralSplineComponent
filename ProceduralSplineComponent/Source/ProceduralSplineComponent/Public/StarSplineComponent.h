e834a380fc6d504d8d30b9c288b9972fa3e14b93
beisaku <98695390+beisaku@users.noreply.github.com>
2022-06-06 21:50:38 +0900

    Add: StarSplineComponent (#16)

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/StarSplineComponent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ProceduralSplineComponentBase.h"
+
+#include "StarSplineComponent.generated.h"
+
+UCLASS(ClassGroup = (ProceduralSpline), meta = (BlueprintSpawnableComponent))
+class PROCEDURALSPLINECOMPONENT_API UStarSplineComponent : public UProceduralSplineComponentBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ProceedSpline() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Star Spline", DisplayName = "N gram", meta = (ClampMin = 3))
+	int32 Ngram = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Star Spline", meta = (ClampMin = 0.0f))
+	float OuterRadius = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Star Spline", meta = (ClampMin = 0.0f))
+	float InnerRadius = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Star Spline", meta = (InlineEditConditionToggle))
+	bool bRoundCorner = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Star Spline",
+		meta = (EditCondition = "bRoundCorner", DisplayName = "Rounded Corner"))
+	FSplineRoundedCornerParams RoundedCornerParams;
+};
