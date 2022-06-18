b485f19375e16ede6fdfa71b3329a6b6148cbac6
beisaku <98695390+beisaku@users.noreply.github.com>
2022-06-18 17:10:19 +0900

    Add: SpringSplineComponent (#18)

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/SpringSplineComponent.cpp
@@ -0,0 +1,28 @@
+#include "SpringSplineComponent.h"
+
+void USpringSplineComponent::ProceedSpline()
+{
+	Super::ProceedSpline();
+
+	ClearSplinePoints();
+
+	for (int32 PointIndex = 0; PointIndex < PointNum; PointIndex++)
+	{
+		const float PointIndexRatio = (float) PointIndex / (PointNum - 1);
+		FVector Location = FVector(Radius * FMath::Sin(PointIndexRatio * TurnNum * 2.0 * PI),
+			Radius * FMath::Cos(PointIndexRatio * TurnNum * 2.0 * PI), FMath::Lerp<float>(0.0, Height, PointIndexRatio));
+		AddSplinePointAtIndex(Location, PointIndex, ESplineCoordinateSpace::Local, false);
+		if (PointIndex == 0 || PointIndex == PointNum - 1)
+		{
+			SetSplinePointType(PointIndex, ESplinePointType::CurveClamped, false);
+		}
+		else
+		{
+			SetSplinePointType(PointIndex, ESplinePointType::Curve, false);
+		}
+	}
+
+	SetClosedLoop(false, false);
+
+	UpdateSpline();
+}
