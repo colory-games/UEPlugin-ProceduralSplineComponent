588f762ff048d0c51e80113cd346c37c08497255
beisaku <98695390+beisaku@users.noreply.github.com>
2022-06-18 20:41:21 +0900

    Add: JaggedSplineComponent (#20)

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/JaggedSplineComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ProceduralSplineComponentBase.h"
+
+#include "JaggedSplineComponent.generated.h"
+
+UCLASS(ClassGroup = (ProceduralSpline), meta = (BlueprintSpawnableComponent))
+class PROCEDURALSPLINECOMPONENT_API UJaggedSplineComponent : public UProceduralSplineComponentBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ProceedSpline() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jagged Spline", meta = (ClampMin = 3))
+	int32 PointNum = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jagged Spline", meta = (ClampMin = 0.0f))
+	float Width = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jagged Spline", meta = (ClampMin = 0.0f))
+	float Length = 50.0f;
+};
