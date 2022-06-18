6aff36a77438ed7273f3b82180cd4bd080598a8a
beisakubei <beisaku1toriaezu@gmail.com>
2022-06-18 17:02:39 +0900

    add spring

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/SpringSplineComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ProceduralSplineComponentBase.h"
+
+#include "SpringSplineComponent.generated.h"
+
+UCLASS(ClassGroup = (ProceduralSpline), meta = (BlueprintSpawnableComponent))
+class PROCEDURALSPLINECOMPONENT_API USpringSplineComponent : public UProceduralSplineComponentBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ProceedSpline() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spring Spline", meta = (ClampMin = 0.0f))
+	float TurnNum = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spring Spline", meta = (ClampMin = 3))
+	int32 PointNum = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spring Spline", meta = (ClampMin = 0.0f))
+	float Radius = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spring Spline", meta = (ClampMin = 0.0f))
+	float Height = 500.0f;
+};
