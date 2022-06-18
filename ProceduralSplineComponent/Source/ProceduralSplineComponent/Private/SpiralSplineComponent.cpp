df8b83616e2492012d46f61fb8aee2c31c385b3a
beisakubei <beisaku1toriaezu@gmail.com>
2022-06-18 17:54:43 +0900

    Add spiral spline

---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/SpiralSplineComponent.cpp
@@ -0,0 +1,31 @@
+#include "SpiralSplineComponent.h"
+
+void USpiralSplineComponent::ProceedSpline()
+{
+	Super::ProceedSpline();
+
+	ClearSplinePoints();
+
+	const float MaxTheta = 2.0f * PI * TurnNum;
+	for (int32 PointIndex = 0; PointIndex < PointNum; PointIndex++)
+	{
+		const float Theta = MaxTheta * PointIndex / (PointNum - 1);
+		const float R = FMath::Lerp<float>(InnerRadius, OuterRadius, Theta / MaxTheta);
+		const FVector Location = (!bInvertSpiral)
+			? FVector(R * FMath::Sin(Theta), R * FMath::Cos(Theta), 0.0f)
+			: FVector(-R * FMath::Sin(Theta), R * FMath::Cos(Theta), 0.0f);
+		AddSplinePointAtIndex(Location, PointIndex, ESplineCoordinateSpace::Local, false);
+		if (PointIndex == 0 || PointIndex == PointNum - 1)
+		{
+			SetSplinePointType(PointIndex, ESplinePointType::CurveClamped, false);
+		}
+		else
+		{
+			SetSplinePointType(PointIndex, ESplinePointType::Curve, false);
+		}
+	}
+
+	SetClosedLoop(false, false);
+
+	UpdateSpline();
+}
---
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Public/SpiralSplineComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ProceduralSplineComponentBase.h"
+
+#include "SpiralSplineComponent.generated.h"
+
+UCLASS(ClassGroup = (ProceduralSpline), meta = (BlueprintSpawnableComponent))
+class PROCEDURALSPLINECOMPONENT_API USpiralSplineComponent : public UProceduralSplineComponentBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ProceedSpline() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral Spline", meta = (ClampMin = 3))
+	int32 PointNum = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral Spline", meta = (ClampMin = 0.0f))
+	float TurnNum = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral Spline", meta = (ClampMin = 0.0f))
+	float InnerRadius = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral Spline", meta = (ClampMin = 0.0f))
+	float OuterRadius = 500.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral Spline")
+    bool bInvertSpiral = false;
+};
