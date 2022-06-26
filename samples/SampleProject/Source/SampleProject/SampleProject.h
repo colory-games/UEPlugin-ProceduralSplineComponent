f3517fe678557b828292ae2c0e4f331f841de44c
nutti <nutti.metro@gmail.com>
2022-06-26 21:13:49 +0900

    Add: Sample project

---
+++ samples/SampleProject/Source/SampleProject/SampleProject.h
@@ -0,0 +1,13 @@
+/*!
+ * SampleProject
+ *
+ * Copyright (c) 2022 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+
