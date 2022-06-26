9da34c359d3caafcd4868211dabafc6e40a8f88a
nutti <nutti.metro@gmail.com>
2022-06-26 20:47:02 +0900

    Add: License

--- ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/SpiralSplineComponent.cpp
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/SpiralSplineComponent.cpp
@@ -1,3 +1,12 @@
+/*!
+ * ProceduralSplineComponent
+ *
+ * Copyright (c) 2022 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+*/
+
 #include "SpiralSplineComponent.h"
 
 void USpiralSplineComponent::ProceedSpline()
