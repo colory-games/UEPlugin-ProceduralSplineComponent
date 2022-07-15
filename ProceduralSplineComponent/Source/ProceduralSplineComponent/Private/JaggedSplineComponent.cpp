a198e8df7682dc1b846a2017f88310a5b32f8e7e
nutti <nutti.metro@gmail.com>
2022-07-16 00:40:39 +0900

    Add: License (#21)

--- ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/JaggedSplineComponent.cpp
+++ ProceduralSplineComponent/Source/ProceduralSplineComponent/Private/JaggedSplineComponent.cpp
@@ -1,3 +1,12 @@
+/*!
+ * ProceduralSplineComponent
+ *
+ * Copyright (c) 2022 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 #include "JaggedSplineComponent.h"
 
 void UJaggedSplineComponent::ProceedSpline()
